561262c0ed1acb2400369ddd943cf006b8e01111
shihkual <shihkual@umich.edu>
2022-04-04 18:35:34 -0400

    Modify the image resolution

--- serial/frensel_visu.py
+++ serial/frensel_visu.py
@@ -6,9 +6,9 @@ import rowan
 import matplotlib.pyplot as plt
 
 pr = signac.get_project()
-
+label = 'init'
 for job in pr.find_jobs():
-    traj = gsd.hoomd.open(name=job.fn('trajectory.gsd'), mode="rb")
+    traj = gsd.hoomd.open(name=job.fn(f'{label}_trajectory.gsd'), mode="rb")
 
 cpu = fresnel.Device(mode='cpu', n=1)
 N_frame = -1
@@ -22,10 +22,11 @@ particle_types = frame.particles.typeid
 
 colors = np.empty((N, 3))
 # Color by typeid
-colors[particle_types == 0] = fresnel.color.linear([.95, 0, 0]) # A type
-colors[particle_types == 1] = fresnel.color.linear([0, .95, 0]) # B type
+colors[particle_types == 0] = fresnel.color.linear([.95, .2, .2]) # A type
+colors[particle_types == 1] = fresnel.color.linear([0.05, .95, .95]) # B type
 
 scene = fresnel.Scene(device=cpu)
+scene.background_color = fresnel.color.linear([0.75, 0.75, 0.75])
 # Spheres for every particle in the system
 geometry = fresnel.geometry.Polygon(
     scene,
@@ -34,8 +35,8 @@ geometry = fresnel.geometry.Polygon(
 )
 geometry.position[:] = frame.particles.position[:, :2]
 geometry.angle[:] = radian_orientation
-geometry.material = fresnel.material.Material(specualr=0.5, roughness=0.9, metal=0.2)
-geometry.outline_width = 0.05
+geometry.material = fresnel.material.Material(specular=0.5, roughness=0.9)
+#geometry.outline_width = 0.05
 
 # use color instead of material.color
 geometry.material.primitive_color_mix = 1.0
@@ -45,10 +46,9 @@ geometry.color[:] = fresnel.color.linear(colors)
 fresnel.geometry.Box(scene, box, box_radius=.07)
 
 scene.camera = fresnel.camera.Orthographic.fit(scene)
-scene.lights = fresnel.light.rembrandt()
-out = fresnel.pathtrace(scene, w=1000, h=1000, light_samples=10)
-fig, ax = plt.subplots(figsize=(10, 10))
+out = fresnel.pathtrace(scene, w=1800, h=1000, light_samples=5)
+fig, ax = plt.subplots(figsize=(9, 5))
 ax.imshow(out[:], interpolation='lanczos')
-ax.set_xticks([])
-ax.set_yticks([])
-plt.show()
+ax.axis('off')
+fig.savefig('test.jpg', dpi=300)
+
