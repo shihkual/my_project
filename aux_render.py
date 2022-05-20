957968d532e2a435393f05784a64090c09e5afe4
shihkual <shihkual@umich.edu>
2022-05-20 15:59:08 -0400

    Add render_chosen for rendering image for single frame

--- aux_render.py
+++ aux_render.py
@@ -163,7 +163,7 @@ def render_with_katic(job):
             sm = plt.cm.ScalarMappable(cmap=cm.viridis, norm=norm)
             divider = make_axes_locatable(ax)
             cax = divider.append_axes("right", size="5%")
-            fig.colorbar(sm, cax=cax).set_label(fr"$|\psi'_{k}|$")
+            fig.colorbar(sm, cax=cax).set_label(fr"$|\psi'_{k}(i)|$")
 
             ax.axis('off')
 
@@ -176,6 +176,88 @@ def render_with_katic(job):
         repeat=False
     )
     ani.save(job.fn(f'{LABEL}_traj_katic_movie.mp4'), fps=int(DISPLAY_FRAMES / 10), dpi=300)
+
+@Project.operation
+@Project.pre.isfile(TRAJECTORY_FN)
+@directives(walltime=12)
+@directives(memory='4G')
+def render_chosen(job):
+    import rowan
+    import fresnel
+    import matplotlib.pyplot as plt
+    import matplotlib.colors as clrs
+    from matplotlib import cm
+    from matplotlib.animation import FuncAnimation
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+    plt.ioff()
+    trajectory = gsd.hoomd.open(job.fn(TRAJECTORY_FN))
+    traj_length = len(trajectory)
+    chosen_frame = np.linspace(0, traj_length-1, 4).astype(np.int32)
+    _, k_atic = get_frame_and_katic(job)
+
+    def plot(frame):
+        ax.cla()
+        for k in k_atic:
+            box = freud.box.Box.from_box(frame.configuration.box)
+            host_idx = np.where(frame.particles.typeid != 2)
+            guest_idx = np.where(frame.particles.typeid == 2)
+            host_position = frame.particles.position[host_idx]
+            system = (box, host_position)
+
+            voro = freud.locality.Voronoi()
+            voro.compute(system)
+            psi = freud.order.Hexatic(k=k, weighted=True)
+            psi.compute(system, neighbors=voro.nlist)
+            order = np.absolute(psi.particle_order)
+
+            N = frame.particles.N
+            orientation = frame.particles.orientation
+            orientation = rowan.to_axis_angle(orientation)
+            radian_orientation = orientation[0][:, 2] * orientation[1]
+
+            colors = np.empty((N, 3))
+            # Color by typeid
+            colors[host_idx] = cm.viridis(order)[:, :3]
+            colors[guest_idx] = fresnel.color.linear([0.0, 0.0, 1.0])
+
+            scene = fresnel.Scene()
+            scene.background_color = fresnel.color.linear([0.75, 0.75, 0.75])
+            # Spheres for every particle in the system
+            geometry = fresnel.geometry.Polygon(
+                scene,
+                N=N,
+                vertices=frame.particles.type_shapes[0]['vertices']
+            )
+            geometry.position[:] = frame.particles.position[:, :2]
+            geometry.angle[:] = radian_orientation
+            geometry.material = fresnel.material.Material(specular=0.5, roughness=0.9)
+            # geometry.outline_width = 0.05
+
+            # use color instead of material.color
+            geometry.material.primitive_color_mix = 1.0
+            geometry.color[:] = fresnel.color.linear(colors)
+
+            # create box in fresnel
+            fresnel.geometry.Box(scene, box, box_radius=.07)
+
+            scene.camera = fresnel.camera.Orthographic.fit(scene)
+            out = fresnel.pathtrace(scene, w=2700, h=1500, light_samples=5)
+            ax.imshow(out[:], interpolation='lanczos')
+
+            norm = clrs.Normalize(vmin=0, vmax=1)
+            sm = plt.cm.ScalarMappable(cmap=cm.viridis, norm=norm)
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%")
+            fig.colorbar(sm, cax=cax).set_label(fr"$|\psi'_{k}(i)|$")
+
+            ax.axis('off')
+
+    for i in chosen_frame:
+        frame = trajectory[i]
+        fig, ax = plt.subplots(1, 1, figsize=(9, 5))
+        plot(frame)
+        fig.savefig(f"{LABEL}_frame{i}.png", dpi=500)
 
 if __name__ == "__main__":
     Project().main()
